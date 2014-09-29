72256ec00f0120b14fd36783407660ddd05c433c
niyaspavil <pavilniyas@gmail.com>
2014-09-29 11:52:50 +0530

    added template directory path

--- tangowithdjango/rango/views.py
+++ tangowithdjango/rango/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says: Hello World ! <a href ='/rango/about'>About</a>")
+     
+  
+    return 
 
 def about(request):
     return HttpResponse("Rango says: Here is about page...  <a href = '/rango/index'>Index</a>")
