4cb679606d31a961f406b97af8a441f2e4edfbb8
niyaspavil <pavilniyas@gmail.com>
2014-09-29 11:48:29 +0530

    created a url file for rango app

---
+++ tangowithdjango/rango/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns,url
+
+from rango import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^index$', views.index, name = 'index'),
+    url(r'^about$', views.about, name = 'about'),
+    )
+ 
+    
