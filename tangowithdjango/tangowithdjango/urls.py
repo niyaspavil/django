2f848454267fb9c825960b993b9d76799438bec3
niyaspavil <pavilniyas@gmail.com>
2014-09-29 11:53:31 +0530

    added url of rango app url to project configration

--- tangowithdjango/tangowithdjango/urls.py
+++ tangowithdjango/tangowithdjango/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tangowithdjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
