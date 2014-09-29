a1bbe4ef18a322b60b8d7d6b233453425fa09022
niyaspavil <pavilniyas@gmail.com>
2014-09-29 15:51:23 +0530

    creating models

--- tangowithdjango/rango/models.py
+++ tangowithdjango/rango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+   
+    name = models.CharField(max_length = 128, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+class page(models.Model):
+
+    category = model.ForiegnKey(Catogory)
+    title = models.CharField(max_length = 128)
+    url = models.UrlField()
+    views = models.IntegerField(default=0)
+  
+    def __unicode__(self):
+         return self.title
