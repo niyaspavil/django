efbcdd87e6d1fb6b48e5a8e919fb38afab43040a
niyaspavil <pavilniyas@gmail.com>
2014-09-30 19:37:05 +0530

    passed catogory list to index.html

--- tangowithdjango/rango/views.py
+++ tangowithdjango/rango/views.py
@@ -12,7 +12,7 @@ def index(request):
      
     context = RequestContext(request)
     
-    category_list = Catogory.objects.order('likes')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {"categories": category_list}
     
     return render_to_response('rango/index.html',context_dict,context)
