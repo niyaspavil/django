f17c1c4908f011e530fedb428f0778ce96f8a3a2
niyaspavil <pavilniyas@gmail.com>
2014-10-01 13:41:45 +0530

    modified index.html

--- tangowithdjango/rango/views.py
+++ tangowithdjango/rango/views.py
@@ -14,6 +14,13 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {"categories": category_list}
     
+    page_list = Page.objects.order_by("-views")[:5]
+    
+    context_dict = {"pages" : page_list}
+
+    for category in category_list:
+	category.url = category.name.replace(" ", "_")
+
     return render_to_response('rango/index.html',context_dict,context)
 
 def about(request):
@@ -27,14 +34,14 @@ def category(request, category_name_url):
 
     context = RequestContext(request)
 
-    category_name = catogory_name_url.replace('_',' ')
+    category_name = category_name_url.replace('_',' ')
 
     context_dict = {'category_name': category_name}
 
     try:
 	
-	category = Category.object,get(name = category_name)
-	pages = Page.object.filter(category=category)
+	category = Category.objects.get(name = category_name)
+	pages = Page.objects.filter(category=category)
 	
 	context_dict ['pages'] = pages
 
