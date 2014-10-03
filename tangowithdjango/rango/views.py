05b04a8d3817a04ca6532b5d8ab9729584fd1cec
niyaspavil <pavilniyas@gmail.com>
2014-10-03 17:46:52 +0530

    added regrtation functionality

--- tangowithdjango/rango/views.py
+++ tangowithdjango/rango/views.py
@@ -12,7 +12,7 @@ from rango.models import Category, Page
 
 # import rango forms
 
-from rango.forms import CategoryForms, PageForms
+from rango.forms import CategoryForms, PageForms, UserForm, UserProfileForm
 
 
 def index(request):
@@ -117,3 +117,40 @@ def add_page(request,category_name_url):
             {'category_name_url':category_name_url,
               'category_name':category_name, 'form':form},
             context)
+
+def register(request):
+
+    context = RequestContext(request)
+    registered =False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit = False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render_to_response(
+            'rango/register.html',
+            {'user_form':user_form,'profile_form':profile_form,
+            'registered':registered}, context)
